34b596ae177d3771b603962fa5fd47e6f1dd0308
Arjun Kashyap <arjuns062009@gmail.com>
2019-01-09 00:46:35 +0530

    final

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+
+namespace DocuVie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            Document document = new Document();
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/Documents/word.docx";
+            document.LoadFromFile(path);
+            document.SaveToFile(AppDomain.CurrentDomain.BaseDirectory + "/Documents/"+"PDF_VERSION.PDF", FileFormat.PDF);
+            ViewBag.IFrame = "~/Documents/" + "PDF_VERSION.PDF";
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
